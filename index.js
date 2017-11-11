e75d3c41e8af77863aea10f0e1d6cba0ab09cc95
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-11 02:49:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nameToAttributes(drivers) {
   const driversObj = {}
   drivers.map(function(driver) {
     const name = driver.split(" ");
-    Object.assign({}, driversObj, {firstName: name[0], lastName: name[1]});
+    return Object.assign({}, {firstName: name[0], lastName: name[1]});
     console.log(drivers);
   });
   console.log(driversObj);
